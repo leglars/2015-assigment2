9f925d93f95037527b6fd15af00724db2a9f9063
Leglars.Zheng <leglars@gmail.com>
2015-09-23 01:27:45 +1000

    root window, menu, open file, check function, close

--- assign2.py
+++ assign2.py
@@ -21,11 +21,124 @@ from assign2_support import *
 
 # Add your code here
 
+from collections import OrderedDict
+
+class AnimalDataPlotApp(object):
+
+    def __init__(self, master):
+
+        self._master = master
+        self._data = AnimalData()
+        self._filename = ""
+
+        master.title("AnimalDataPlotApp")
+
+        #create the menu
+        menu_bar = tk.Menu(master)
+        master.config(menu = menu_bar)
+
+        file_menu = tk.Menu(menu_bar)
+        menu_bar.add_cascade(label="File", menu=file_menu)
+        file_menu.add_command(label="Open", command=self.open_file)
+
+        #close window
+        master.protocol("WM_DELETE_WINDOW", self.close)
+
+        self._Plotter = Plotter(master, self._data)
+        self._Plotter.pack(side = tk.RIGHT, fill = tk.Y, expand = False)
+
+
+    def open_file(self):
+        self._filename = filedialog.askopenfilename()
+
+        if self.check(self._filename):
+            self._data.load_data(self._filename)
+            self._Plotter.plot_animal_data()
+            self._SelectionBox.add_button()
+
+
+    def check(self, filename):
+        base = os.path.basename(filename)
+        if not base.endswith('.txt'):
+            if base:
+                msg = filename + " is not a suitable data file"
+                messagebox.showerror(title="File Error", message=msg)
+                return False
+            else:
+                return False
+
+        animal = base.replace(".txt", "")
+        animals = self._data.get_animal_names()
+        if len(animals) != 0:
+            if animal in animals:
+                msg = "The data of " + animal + " has been loaded"
+                messagebox.showwarning(title="Re-loading data file", message=msg)
+                return False
+        return True
+
+
+    def close(self):
+        self._master.destroy()
+
+class AnimalData ():
+
+
+    def __init__(self):
+        pass
+
+
+    def load_data(self, filename):
+        pass
+
+
+    def get_animal_names(self):
+        pass
+
+
+    def get_animal(self, animal):
+        pass
+
+
+    def is_selected(self, index):
+        pass
+
+
+    def select(self, index):
+        pass
+
+
+    def deselect(self, index):
+        pass
+
+
+    def get_ranges(self):
+        pass
+
+
+    def to_tabbed_string(self, index):
+        pass
+
+
+
+class Plotter():
+
+    def __init__(self):
+        pass
+
+
+
+class SelectionBox():
+
+    def __init__(self):
+        pass
 
 
 
 
+class SummaryWindow():
 
+    def __init__(self):
+        pass
 
 
 
